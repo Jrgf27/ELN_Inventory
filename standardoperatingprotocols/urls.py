d956d86a4b62b0451958cd06272d4f9914543ab9
Joao Fernandes <ricardo.jfernandes27@gmail.com>
2023-09-19 23:34:03 +0100

    Transfered logic from edit to other functions

--- standardoperatingprotocols/urls.py
+++ standardoperatingprotocols/urls.py
@@ -7,15 +7,15 @@ urlpatterns = [
     path('<int:id>', views.SpecificSOP.as_view(), name = 'specificSOP'),
     path('edit/<int:id>', views.EditSOP.as_view(), name = 'editSOP'),
 
-    path('htmx/create-attachment-form/<int:id>', views.CreateAttachmentForm, name='create-attachment-form_SOP'),
+    path('htmx/create-attachment-form/<int:id>', views.CreateAttachmentForm.as_view(), name='create-attachment-form_SOP'),
     path('htmx/attachment/<int:id>/<int:attachmentId>', views.SpecificAttachment, name='specificAttachment_SOP'),
     path('htmx/attachment/<int:id>/<int:attachmentId>/delete', views.DeleteAttachment, name='deleteAttachment_SOP'),
 
-    path('htmx/create-trainer-form/<int:id>', views.CreateSOPTrainerForm, name='create-SOPTrainer-form'),
+    path('htmx/create-trainer-form/<int:id>', views.CreateSOPTrainerForm.as_view(), name='create-SOPTrainer-form'),
     path('htmx/trainer/<int:SOPId>/<int:userId>/', views.SpecificSOPTrainer, name='specificSOPTrainer'),
     path('htmx/trainer/<int:SOPId>/<int:userId>/delete', views.DeleteSOPTrainer, name='deleteSOPTrainer'),
 
-    path('htmx/create-trainee-form/<int:id>', views.CreateSOPTraineeForm, name='create-SOPTrainee-form'),
+    path('htmx/create-trainee-form/<int:id>', views.CreateSOPTraineeForm.as_view(), name='create-SOPTrainee-form'),
     path('htmx/trainee/<int:SOPId>/<int:userId>/', views.SpecificSOPTrainee, name='specificSOPTrainee'),
     path('htmx/trainee/<int:SOPId>/<int:userId>/delete', views.DeleteSOPTrainee, name='deleteSOPTrainee'),
 ]
