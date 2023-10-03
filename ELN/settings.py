91f48913888bdd6e4a6f4d5c3659c7eee688ce9d
Joao Fernandes <71797176+Jrgf27@users.noreply.github.com>
2023-10-03 20:31:30 +0100

    Update settings.py

--- ELN/settings.py
+++ ELN/settings.py
@@ -184,20 +184,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL="/"
 LOGOUT_REDIRECT_URL="/login"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER= 'djangoELN@gmail.com'
-EMAIL_HOST_PASSWORD = 'euyveziixqcwyizw'
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST='smtp.office365.com'
-# EMAIL_PORT=587
-# EMAIL_USE_TLS=True
-# EMAIL_HOST_USER= 'joao@cellcraft.com'
-# EMAIL_HOST_PASSWORD = 
-# SERVER_EMAIL = EMAIL_HOST_USER
-
 
 LOGIN_URL = '/login/'
