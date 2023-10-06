1304075c064bfd0d3519331eb8cb43c942c40f04
Joao Fernandes <ricardo.jfernandes27@gmail.com>
2023-10-06 15:02:43 +0100

    Test new dockerfile

--- stock/models.py
+++ stock/models.py
@@ -1,24 +1,26 @@
 from django.db import models
 from supplier.models import SuppliersItems
 from location.models import Locations
-from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Stock(models.Model):
+    """Class representing the Stock Models"""
+
     batchCode = models.CharField(max_length=200, blank=True, null=True)
     quantity = models.IntegerField()
     hasBatchCode = models.BooleanField()
     locationId = models.ForeignKey(Locations, on_delete=models.SET_NULL, null=True)
     itemId = models.ForeignKey(SuppliersItems, on_delete=models.CASCADE, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)  
     isEnabled=models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.itemId.itemId.name + " - " + self.batchCode
-    
+
 class Stock_Versions(models.Model):
+    """Class representing the Stock Versions Models"""
+
     stock = models.ForeignKey(Stock, on_delete=models.SET_NULL, null=True)
     batchCode = models.CharField(max_length=200, blank=True, null=True)
     quantity = models.IntegerField()
@@ -30,6 +32,6 @@ class Stock_Versions(models.Model):
     lastAction = models.CharField(max_length=10)
     lastEditedUserSignature = models.CharField(max_length=200, default="")
     lastEditedDate = models.DateField(auto_now_add=True)
-
+ 
     def __str__(self) -> str:
         return self.itemId.itemId.name + " - " + self.batchCode
